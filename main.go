0e44acc1d1d772c1c60589f3dd99685e40010393
Anuj <mailanujhere@gmail.com>
2025-03-23 01:01:37 +0530

    very basics

--- main.go
+++ main.go
@@ -12,7 +12,25 @@ func add(x, y int) int {
 	return x + y
 }
 
+// returns multiple values
+func split(sum int) (int, int) {
+	//can only init like this in funcs
+	x := sum * 4 / 9
+	y := sum - x
+
+	return x, y
+}
+
+// variable initialization outside functions
+// vars are initialized to the dtype zero values no nulls
+var a, b, c bool
+var d string = "Anuj"
+
 func main() {
 	fmt.Println("Hello World ", rand.Intn(10))
 	fmt.Println(math.Pi)
+
+	fmt.Println(add(10, 12))
+	fmt.Println(split(17))
+	fmt.Println(a, b, c, d)
 }
